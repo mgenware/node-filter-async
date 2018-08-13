966f4c410cbfe825236e8c3c6af9193526636f24
Mgen <m@mgenware.com>
2018-08-13 21:51:04 +0800

    Make code more concise

--- lib/main.ts
+++ lib/main.ts
@@ -1,14 +1,8 @@
 export async function filterAsync<T>(
   array: T[],
   callback: (value: T, index: number) => Promise<boolean>): Promise<T[]> {
-    const bools: boolean[] = await Promise.all(array.map(async (value, index) => {
+    const tests: boolean[] = await Promise.all(array.map(async (value, index) => {
       return await callback(value, index);
     }));
-    const results: T[] = [];
-    for (let i = 0; i < array.length; i++) {
-      if (bools[i]) {
-        results.push(array[i]);
-      }
-    }
-    return results;
+    return array.filter((_, i) => tests[i]);
 }
