376761ba0e0241bae0283d1ad37484e768094330
Mgen <m@mgenware.com>
2018-10-25 16:33:28 +0800

    Check empty arguments

--- lib/main.ts
+++ lib/main.ts
@@ -1,8 +1,17 @@
+function checkArgument(value: unknown, name: string) {
+  if (!value) {
+    throw new Error(`The argument "${name}" cannot be empty`);
+  }
+}
+
 export async function filterAsync<T>(
   array: T[],
   callback: (value: T, index: number) => Promise<boolean>): Promise<T[]> {
-    const tests: boolean[] = await Promise.all(array.map(async (value, index) => {
+    checkArgument(array, 'array');
+    checkArgument(callback, 'callback');
+
+    const results: boolean[] = await Promise.all(array.map(async (value, index) => {
       return await callback(value, index);
     }));
-    return array.filter((_, i) => tests[i]);
+    return array.filter((_, i) => results[i]);
 }
